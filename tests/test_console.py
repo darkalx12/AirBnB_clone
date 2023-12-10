2e8dc3b819abcbc80a9f823907a290386b54b4f7
darkalx12 <darkword907@gmail.com>
2023-12-10 05:20:21 -0800

    test

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,66 +1,103 @@
 #!/usr/bin/python3
-"""
-Unit tests for console using Mock module from python standard library
-Checks console for capturing stdout into a StringIO object
-"""
+"""Module for TestHBNBCommand class."""
 
-import os
-import sys
-import unittest
-from unittest.mock import create_autospec, patch
-from io import StringIO
 from console import HBNBCommand
-from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+from models.engine.file_storage import FileStorage
+import unittest
+import datetime
+from unittest.mock import patch
+import sys
+from io import StringIO
+import re
+import os
 
 
-class TestConsole(unittest.TestCase):
+class TestHBNBCommand(unittest.TestCase):
     """
-    Unittest for the console model
+        Tests HBNBCommand console
+
     """
 
     def setUp(self):
-        """Redirecting stdin and stdout"""
-        self.mock_stdin = create_autospec(sys.stdin)
-        self.mock_stdout = create_autospec(sys.stdout)
-        self.err = ["** class name missing **",
-                    "** class doesn't exist **",
-                    "** instance id missing **",
-                    "** no instance found **",
-                    ]
+        print("Testing Console methods")
 
-        self.cls = ["BaseModel",
-                    "User",
-                    "State",
-                    "City",
-                    "Place",
-                    "Amenity",
-                    "Review"]
+    def test_help_quit(self):
+        """Tests the help command."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help quit")
+        s = 'Quit command to exit the program\n'
+        self.assertEqual(s, f.getvalue())
 
-    def create(self, server=None):
-        """
-        Redirects stdin and stdout to the mock module
-        """
-        return HBNBCommand(stdin=self.mock_stdin, stdout=self.mock_stdout)
+    def test_help_create(self):
+        """Tests the create command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help create")
+        s = 'Creates a new instance\n'
+        self.assertEqual(s, f.getvalue())
 
-    def last_write(self, nr=None):
-        """Returns last n output lines"""
-        if nr is None:
-            return self.mock_stdout.write.call_args[0][0]
-        return "".join(map(lambda c: c[0][0],
-                           self.mock_stdout.write.call_args_list[-nr:]))
+    def test_help_clear(self):
+        """Tests default"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help clear")
+            s = 'Clear the screen\n'
+            self.assertEqual(s, f.getvalue())
 
-    def test_quit(self):
-        """Quit command"""
-        cli = self.create()
-        self.assertTrue(cli.onecmd("quit"))
+    def test_help_show(self):
+        """Tests show"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help show")
+            s = 'prints the string representation of an instance\n'
+            self.assertEqual(s, f.getvalue())
+
+    def test_help_destroy(self):
+        """Tests destroy"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help destroy")
+            s = 'Deletes an instance based on the class name and id\n'
+            self.assertEqual(s, f.getvalue())
+
+    def test_help_all(self):
+        """Tests All"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help all")
+            s = 'Prints all string representation of all instances\n'
+            self.assertEqual(s, f.getvalue())
+
+    def test_help_update(self):
+        """Tests Update"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help update")
+            s = ' Updates the class\n'
+            self.assertEqual(s, f.getvalue())
+
+    def test_help_count(self):
+        """Tests count"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help count")
+            s = 'counts\n'
+            self.assertEqual(s, f.getvalue())
+
+    def test_help_EOF(self):
+        """End of line"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help EOF")
+            s = 'Handles EOF\n'
+            self.assertEqual(s, f.getvalue())
+
+    def test_help_emptyline(self):
+        """Tests empty line"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help emptyline")
+            s = '*** No help on emptyline\n'
+            self.assertEqual(s, f.getvalue())
+
+    def test_help_default(self):
+        """default"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help default")
+            s = '*** No help on default\n'
+            self.assertEqual(s, f.getvalue())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
